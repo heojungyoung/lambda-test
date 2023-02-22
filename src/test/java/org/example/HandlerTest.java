834105e9d0af4ba43598d065a98be06a72fafd76
heojungyoung <tyireal@gmail.com>
2023-02-22 22:55:42 +0900

    test

---
+++ src/test/java/org/example/HandlerTest.java
@@ -0,0 +1,97 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.ClientContext;
+import com.amazonaws.services.lambda.runtime.CognitoIdentity;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(classes = main.class)
+public class HandlerTest {
+
+
+    @Test
+    void aaa(){
+
+        Handler dd = new Handler();
+        Context context = new Context() {
+            @Override
+            public String getAwsRequestId() {
+                return "abc";
+            }
+
+            @Override
+            public String getLogGroupName() {
+                return null;
+            }
+
+            @Override
+            public String getLogStreamName() {
+                return null;
+            }
+
+            @Override
+            public String getFunctionName() {
+                return null;
+            }
+
+            @Override
+            public String getFunctionVersion() {
+                return null;
+            }
+
+            @Override
+            public String getInvokedFunctionArn() {
+                return null;
+            }
+
+            @Override
+            public CognitoIdentity getIdentity() {
+                return null;
+            }
+
+            @Override
+            public ClientContext getClientContext() {
+                return null;
+            }
+
+            @Override
+            public int getRemainingTimeInMillis() {
+                return 0;
+            }
+
+            @Override
+            public int getMemoryLimitInMB() {
+                return 0;
+            }
+
+            @Override
+            public LambdaLogger getLogger() {
+                return new LambdaLogger() {
+                    @Override
+                    public void log(String message) {
+
+                    }
+
+                    @Override
+                    public void log(byte[] message) {
+
+                    }
+                };
+            }
+        }  ;
+
+        Map<String,String> event = new HashMap<>();
+
+
+
+        dd.handleRequest(event, context);
+
+    }
+
+
+}
